bae72c4390abfc0ada96ef12faa123bdbde32835
sanjayautomation <treestechnospace@gmail.com>
2023-06-22 21:03:42 +0530

    first time

---
+++ src/main/java/PageLayer/LoginPage.java
@@ -0,0 +1,9 @@
+package PageLayer;
+
+public class LoginPage {
+	
+	public void login() {
+		System.out.println("LoginPage");//Sanjay
+	}
+
+}
---
+++ src/test/java/TestLayer/LoginPageTest.java
@@ -0,0 +1,13 @@
+package TestLayer;
+
+import PageLayer.LoginPage;
+
+public class LoginPageTest {
+
+	public static void main(String[] args) {
+		LoginPage login1=new LoginPage();
+		login1.login();
+
+	}
+
+}
